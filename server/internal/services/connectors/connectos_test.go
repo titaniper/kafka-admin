462fad59aa808a0ff7dca3a7c542a2e5ae4b8595
titaniper <flutter.kr@gmail.com>
2024-08-27 15:40:33 +0900

    refactor: minor

--- server/internal/services/connectors/connectos_test.go
+++ server/internal/services/connectors/connectos_test.go
@@ -1,6 +1,7 @@
 package connectors
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -14,7 +15,7 @@ const (
 
 // TODO: 개선 ㅋㅋ
 func Test_GET_CONNECTOR(t *testing.T) {
-	client := New("http://localhost:51541")
+	client := New("http://localhost:56960/")
 
 	response, _ := client.GetAllConnector()
 	for _, name := range response.Connectors {
@@ -24,18 +25,20 @@ func Test_GET_CONNECTOR(t *testing.T) {
 		if len(connector.Tasks) > 0 {
 			taskStatus := statusResponse.TasksStatus[0]
 			if taskStatus.State == "FAILED" {
-				println("Metadata error", connector.Name)
+				println("connector.Name", connector.Name)
+				errorType := 0
 				if strings.Contains(taskStatus.Trace, " An exception occurred in the change event producer. This connector will be stopped.") {
-					println("1 Metadata error", connector.Name)
-					client.RestartTask(connector.Name, taskStatus.ID)
+					errorType = 1
 				} else if strings.Contains(taskStatus.Trace, "The database schema history couldn't be recovered. Consider to increase the value for schema.history.internal.kafka.recovery.poll.interval.ms") {
-					println("2 Metadata error", connector.Name)
-					client.RestartTask(connector.Name, taskStatus.ID)
+					errorType = 2
 				} else if strings.Contains(taskStatus.Trace, "java.lang.OutOfMemoryError: Java heap space") {
-					println("3 java.lang.OutOfMemoryError: Java heap space", connector.Name)
+					errorType = 3
+				}
+
+				if errorType > 0 {
+					fmt.Printf("type %d", errorType)
+					println("trace", taskStatus.Trace)
 					client.RestartTask(connector.Name, taskStatus.ID)
-				} else {
-					println("Another", connector.Name)
 				}
 			}
 		}
